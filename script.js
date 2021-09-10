7bde042f7ab64feb69c5754b3646e9952441b0bb
unknown <tranduclong0205@gmail.com>
2021-09-10 10:58:30 +0700

    Edit

--- script.js
+++ script.js
@@ -1,64 +1,66 @@
 /*----------Address-form--------*/
-const addressbtn = document.querySelector('#address-form');
-const addressclose = document.querySelector('#address-close');
-addressbtn.addEventListener("click", function() {
-    document.querySelector('.address-form').style.display = "flex";
-})
-addressclose.addEventListener("click", function() {
-    document.querySelector(".address-form").style.display = "none";
-})
-
-/*---------------Slider---------------*/
-const rightbtn = document.querySelector('.fa-chevron-right');
-const leftbtn = document.querySelector('.fa-chevron-left');
-let index = 0;
-const imgnumber = document.querySelectorAll('.slider-content-left-top img');
-
-rightbtn.addEventListener("click", function() {
-    index = index + 1;
-    if (index > imgnumber.length - 1) {
-        index = 0;
-    }
-    document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
-})
-
-leftbtn.addEventListener("click", function() {
-    index = index - 1;
-    if (index < 0) {
-        index = imgnumber.length - 1;
-    }
-    document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
-})
-
-/*-------------Slider1----------------*/
-const imgcntslide = document.querySelectorAll('.slider-content-left-bottom li');
-
-imgcntslide.forEach(function(imgName, index) {
-    imgName.addEventListener("click", function() {
-        document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
-        imgName.classList.add("active");
-
+{
+    const addressbtn = document.querySelector('#address-form');
+    const addressclose = document.querySelector('#address-close');
+    addressbtn.addEventListener("click", function() {
+        document.querySelector('.address-form').style.display = "flex";
     })
-})
+    addressclose.addEventListener("click", function() {
+        document.querySelector(".address-form").style.display = "none";
+    })
 
-function removeActive() {
-    let imgActive = document.querySelector('.active');
-    imgActive.classList.remove("active");
+    /*---------------Slider---------------*/
+    const rightbtn = document.querySelector('.fa-chevron-right');
+    const leftbtn = document.querySelector('.fa-chevron-left');
+    let index = 0;
+    const imgnumber = document.querySelectorAll('.slider-content-left-top img');
 
-}
-/*---------------slider auto----------------*/
-function imgAuto() {
-    index = index + 1;
-    if (index > imgnumber.length - 1) {
-        index = 0;
+    rightbtn.addEventListener("click", function() {
+        index = index + 1;
+        if (index > imgnumber.length - 1) {
+            index = 0;
+        }
+        document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
+    })
+
+    leftbtn.addEventListener("click", function() {
+        index = index - 1;
+        if (index < 0) {
+            index = imgnumber.length - 1;
+        }
+        document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
+    })
+
+    /*-------------Slider1----------------*/
+    const imgcntslide = document.querySelectorAll('.slider-content-left-bottom li');
+
+    imgcntslide.forEach(function(imgName, index) {
+        imgName.addEventListener("click", function() {
+            document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
+            imgName.classList.add("active");
+
+
+        })
+    })
+
+    function removeActive() {
+        let imgActive = document.querySelector('.active');
+        imgActive.classList.remove("active");
+
     }
-    removeActive();
-    document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
-    imgcntslide[index].classList.add("active")
+    /*---------------slider auto----------------*/
+    function imgAuto() {
+        index = index + 1;
+        if (index > imgnumber.length - 1) {
+            index = 0;
+        }
+        removeActive();
+        document.querySelector(".slider-content-left-top").style.right = index * 100 + "%";
+        imgcntslide[index].classList.add("active")
 
 
 
+    }
+    setInterval(imgAuto, 2000);
 }
-setInterval(imgAuto, 2000);
-
-/*---------------*/
+/*--------------Category  Laptophoctap---------------*/
